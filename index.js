92b0d084192489ee4cf8979b24c0e947add2acfc
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-02-11 14:46:11 +0200

    T3.17* PUT voi nyt päivittää tietoja id:n kautta

--- index.js
+++ index.js
@@ -145,6 +145,21 @@ app.post('/api/persons', (request,response,next) => {
 })
 
 
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+
+    const person = {
+        name: body.name,
+        number: body.number,
+    }
+
+    Person.findByIdAndUpdate(request.params.id, person, { new: true})
+        .then(updatedPerson => {
+            response.json(updatedPerson)
+        })
+        .catch(error => next(error))
+})
+
 app.use(errorHandler)
 app.use(unknowEndpoint)
 
