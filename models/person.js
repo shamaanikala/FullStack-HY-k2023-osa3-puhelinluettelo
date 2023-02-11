123e7a10a162570c52c32edc8bef503bd2630a5d
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-02-11 20:13:41 +0200

    T3.20* oma custom validointi toimii

--- models/person.js
+++ models/person.js
@@ -26,8 +26,13 @@ const personSchema = new mongoose.Schema({
         required: true,
         // Mallia täältä:
         // https://mongoosejs.com/docs/validation.html#custom-validators
+        // regexp cheat sheet:
+        // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions/Cheatsheet
+        // x{n,} -  Where "n" is a positive integer, matches at least "n" occurrences of the preceding item "x". For example, /a{2,}/ doesn't match the "a" in "candy", but matches all of the a's in "caandy" and in "caaaaaaandy". 
+        // x{n,m} -  Where "n" is 0 or a positive integer, "m" is a positive integer, and m > n, matches at least "n" and at most "m" occurrences of the preceding item "x". For example, /a{1,3}/ matches nothing in "cndy", the "a" in "candy", the two "a"'s in "caandy", and the first three "a"'s in "caaaaaaandy". Notice that when matching "caaaaaaandy", the match is "aaa", even though the original string had more "a"s in it. 
         validate: {
-            validator: v => /\d{3}-\d{7}/.test(v),
+            //validator: v => /\d{3}-\d{7}/.test(v), // mongoose malli
+            validator: v => /^\d{2,3}-\d{7,}/.test(v),
             message: props => `${props.value} is not a valid phone number (custom error)`
         }
     }
